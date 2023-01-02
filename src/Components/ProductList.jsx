182c4680de1a6ec70730de46308cc9b5f1a1134a
isha-paliwal3 <1032201235@mitwpu.edu.in>
2023-01-02 11:15:39 +0530

    added list and grid view for ProductPage

---
+++ src/Components/ProductList.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useFilterContext } from '../Context/FilterContext'
+import GridView from './GridView';
+import ListView from './ListView';
+
+const ProductList = () => {
+  
+  const {filter_products, grid_view} = useFilterContext();
+
+  if(grid_view === true){
+    return (
+    <GridView products = {filter_products}/>
+    )
+  }
+
+  if(grid_view === false){
+    return (
+      <>     
+    <ListView products = {filter_products}/>
+    <h1>heel</h1>
+      </>
+    )
+  }
+
+}
+
+export default ProductList
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AppProvider } from './Context/ProductContext';
+import { FilterContextProvider } from './Context/FilterContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <FilterContextProvider>
+        <App />
+      </FilterContextProvider>
     </AppProvider>
   </React.StrictMode>
 );
